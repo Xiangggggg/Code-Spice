a0ac72942728f507c0539ca8a70e50e9e045d4a2
Xiangggggg <xiangm240@gmail.com>
2021-06-22 10:33:24 -0400

    Co-authored-by: somthecoder <somthecoder@users.noreply.github.com>

--- Code;Spice/main.js
+++ Code;Spice/main.js
@@ -27,11 +27,28 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-void function() { 
-  const $burger = $(".navbar-burger")
-  if ($burger)
-    $burger.click(() => {
-      $(".navbar-menu").toggleClass("is-active");
-      $burger.toggleClass("is-active");
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
     });
-}
+  }
+
+});
