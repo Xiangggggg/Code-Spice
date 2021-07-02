1103bb5c29877680a52447c05ec0f4af65af4350
Xiangggggg <xiangm240@gmail.com>
2021-07-02 14:15:26 -0400

    py routes

---
+++ Code;Spice/routes.py
@@ -0,0 +1,20 @@
+from flask import *
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def home():
+    return render_template("index.html", title="Home")
+
+@main.route("/home")
+def redirect_home():
+    return redirect(url_for("main.home"))
+
+@main.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+@main.route("/signup")
+def signup():
+    return render_template("signup.html", title="Sign Up!")
